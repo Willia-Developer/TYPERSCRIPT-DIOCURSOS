68d161eae0b63d2e87547a4f8ac9c20430ed9c24
Willia Developer <circuitbreak2020@gmail.com>
2022-07-21 18:27:11 -0300

    commit

---
+++ validacao.js
@@ -0,0 +1,15 @@
+/*let button = document.getElementById('button');
+let input1 = document.getElementById('input1');
+let input2 = document.getElementById('input2');
+
+function somarnumeros(numero1, numero2) {
+  if(typeof numero1 === 'Number' && typeof numero2 === 'Number') {
+    return numero1 + numero2
+  } else {
+
+    return Number(numero1) + Number(numero2)
+  }
+}
+button.addEventListener("click",() => {
+  console.log(somarnumeros(input1.value, input2.value));
+})*/
